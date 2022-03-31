b381688da033345a6047dc1274e8c4aede1c8720
ari <arifr1234@gmail.com>
2022-03-31 17:32:12 +0300

    load_web_gpu

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>
 }
 
+var adapter = null;
+var device = null;
+
+function load_web_gpu(){
+  if (!navigator.gpu) throw Error("WebGPU not supported.");
+
+  return navigator.gpu.requestAdapter()
+  .then((_adapter) => {
+    if (!_adapter) throw Error("Couldn’t request WebGPU adapter.");
+    adapter = _adapter
+    return _adapter
+  })
+  .then((adapter) => adapter.requestDevice())
+  .then((_device) => {
+    if (!_device) throw Error("Couldn’t request WebGPU logical device.");
+    device = _device
+  });
+}
+
+function main(){
+  load_web_gpu()
+  .then(() => {
+    
+  })
+}
+
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
-
-  const [likes, setLikes] = useState(0)
-
-  function handleClick() {
-    setLikes(likes + 1)
-  }
+  useEffect(main);
 
   return (
     <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
-      <ul>
-        {names.map(name => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
+      <Header title="🚀" />
     </div>
   )
 }
